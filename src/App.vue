45bb6447d6f706999f862ceab76e525848b2ea99
Mario Blažeka <mblazeka@student.unipu.hr>
2026-09-05 12:59:45 +0200

    Postavi osnovu Vue 2 projekta

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+  background-color: #1a1a1a;
+  color: #111111;
+  font-family: 'Inter', 'Segoe UI', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+  min-height: 100%;
+  background-color: #1a1a1a;
+}
+</style>
